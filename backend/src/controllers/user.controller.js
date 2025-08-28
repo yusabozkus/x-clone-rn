a0290879f15555788f0502af661cd5dd3201d3eb
yusabozkus <yusa4456bozkus@gmail.com>
2025-08-28 23:28:02 +0300

    Update user route codes

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -2,7 +2,8 @@ import asyncHandler from "express-async-handler";
 import User from "../models/user.model.js";
 import Notification from "../models/notification.model.js";
 
-import { clerkClient, getAuth } from "@clerk/express";
+import { getAuth } from "@clerk/express";
+import { clerkClient } from "@clerk/express";
 
 export const getUserProfile = asyncHandler(async (req, res) => {
   const { username } = req.params;
@@ -15,9 +16,7 @@ export const getUserProfile = asyncHandler(async (req, res) => {
 export const updateProfile = asyncHandler(async (req, res) => {
   const { userId } = getAuth(req);
 
-  const user = await User.findOneAndUpdate({ clerkId: userId }, req.body, {
-    new: true,
-  });
+  const user = await User.findOneAndUpdate({ clerkId: userId }, req.body, { new: true });
 
   if (!user) return res.status(404).json({ error: "User not found" });
 
@@ -28,10 +27,9 @@ export const syncUser = asyncHandler(async (req, res) => {
   const { userId } = getAuth(req);
 
   const existingUser = await User.findOne({ clerkId: userId });
-  if (existingUser)
-    return res
-      .status(200)
-      .json({ user: existingUser, message: "User already exists" });
+  if (existingUser) {
+    return res.status(200).json({ user: existingUser, message: "User already exists" });
+  }
 
   const clerkUser = await clerkClient.users.getUser(userId);
 
@@ -62,26 +60,25 @@ export const followUser = asyncHandler(async (req, res) => {
   const { userId } = getAuth(req);
   const { targetUserId } = req.params;
 
-  if (userId === targetUserId)
-    return res.status(400).json({ error: "You cannot follow yourself" });
+  if (userId === targetUserId) return res.status(400).json({ error: "You cannot follow yourself" });
 
   const currentUser = await User.findOne({ clerkId: userId });
   const targetUser = await User.findById(targetUserId);
 
-  if (!currentUser || !targetUser)
-    return res.status(404).json({ error: "User not found" });
+  if (!currentUser || !targetUser) return res.status(404).json({ error: "User not found" });
 
   const isFollowing = currentUser.following.includes(targetUserId);
 
   if (isFollowing) {
+    // unfollow
     await User.findByIdAndUpdate(currentUser._id, {
       $pull: { following: targetUserId },
     });
-
     await User.findByIdAndUpdate(targetUserId, {
       $pull: { followers: currentUser._id },
     });
   } else {
+    // follow
     await User.findByIdAndUpdate(currentUser._id, {
       $push: { following: targetUserId },
     });
@@ -92,9 +89,11 @@ export const followUser = asyncHandler(async (req, res) => {
     await Notification.create({
       from: currentUser._id,
       to: targetUserId,
-      tpye: "follow",
+      type: "follow",
     });
   }
 
-  res.status(200).json({ message: isFollowing ? "User unfollowed successfully" : "User followed successfully"})
+  res.status(200).json({
+    message: isFollowing ? "User unfollowed successfully" : "User followed successfully",
+  });
 });
--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,16 +1,20 @@
 import express from "express";
-import { followUser, getCurrentUser, getUserProfile, syncUser, updateProfile } from "../controllers/user.controller.js";
+import {
+  followUser,
+  getCurrentUser,
+  getUserProfile,
+  syncUser,
+  updateProfile,
+} from "../controllers/user.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-// public route
 router.get("/profile/:username", getUserProfile);
 
-// protected routes
 router.post("/sync", protectRoute, syncUser);
-router.post("/me", protectRoute, getCurrentUser);
+router.get("/me", protectRoute, getCurrentUser);
 router.put("/profile", protectRoute, updateProfile);
-router.put("/follow/:targetUserId", protectRoute, followUser);
+router.post("/follow/:targetUserId", protectRoute, followUser);
 
 export default router;
